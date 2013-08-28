4b8744f0ed50c45f59f2fd8adbb3c7d61ce3cfe2
Vladimir Kirillov <proger@wilab.org.ua>
2013-08-28 18:05:31 +0300

    added expm package

---
+++ package.head.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "erlfsmon", description: "Erlang filesystem event watcher for Linux and OSX",
+                 version: :head, keywords: ["Erlang", "OSX", "Linux", "filesystem events"], 
+                   maintainers: [[name: "Vladimir Kirillov", email: "proger@hackndev.com"]],
+                     repositories: [[github: "proger/erlfsmon"]],
+                     dependencies: [])
