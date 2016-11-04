c643e781ee8c90649e2642624109cc4c3b306605
Vlad Ki <proger@wilab.org.ua>
2016-11-04 10:55:06 +0200

    Delete package.head.exs
    expm is no longer a thing

--- package.head.exs
+++
@@ -1,5 +0,0 @@
-Expm.Package.new(name: "erlfsmon", description: "Erlang filesystem event watcher for Linux and OSX",
-                 version: :head, keywords: ["Erlang", "OSX", "Linux", "filesystem events"], 
-                   maintainers: [[name: "Vladimir Kirillov", email: "proger@hackndev.com"]],
-                     repositories: [[github: "proger/erlfsmon"]],
-                     dependencies: [])
