a4f19f160b2094516011fbde792274aa8c1dd949
Vlad Ki <vlad@kirillov.im>
2016-11-04 11:44:38 +0200

    add support for fswatch (https://github.com/emcrisostomo/fswatch) backend
    no need for fdlinking, idea by @josevalim https://github.com/synrc/fs/pull/6

---
+++ src/fswatch.erl
@@ -0,0 +1,19 @@
+-module(fswatch).
+-export([find_executable/0, start_port/2, known_events/0, line_to_event/1]).
+
+find_executable() ->
+    os:find_executable("fswatch").
+
+start_port(Path, Cwd) ->
+    Args = ["-c", "$0 \"$@\" & PID=$!; read a; kill $PID",
+            "fswatch", "--format=%p\t%f", "--event-flag-separator", ",", Path],
+    erlang:open_port({spawn_executable, os:find_executable("sh")},
+        [stream, exit_status, {line, 16384}, {args, Args}, {cd, Cwd}]).
+
+known_events() -> meh.
+
+line_to_event(Line) ->
+    [Path, Flags1] = string:tokens(Line, [$\t]),
+    Flags2 = string:tokens(Flags1, [$,]),
+    % trust fswatch
+    {Path, [list_to_atom(F) || F <- Flags2]}.
